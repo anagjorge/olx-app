23ccbd69b97ba8f78d0856d5f6add3723fb1a0d6
anajorge <anajessicadegj@gmail.com>
2022-12-07 11:57:58 -0300

    first commit

--- app/src/main/java/com/cursoandroid/olxapp/api/NotificacaoService.java
+++ app/src/main/java/com/cursoandroid/olxapp/api/NotificacaoService.java
@@ -10,7 +10,7 @@ import retrofit2.http.POST;
 public interface NotificacaoService {
 
     @Headers({
-            "Authorization:key=AAAA7PRhs78:APA91bHOuBlvUkYfB0u-rCjYGHRq1WZde9N2hVOJFcB8UXtyIPqCqSF-ifG1VNgyeTTSv_Y2muXKbYk19MLaFXmNBOSk8voCf1I8CNQm4xlgtncvgUtpSqr181CUfRni53sYYJ9w1nmf",
+            "Authorization:key=YOUR_API_KEY",
             "Content-Type:application/json"
 
     })
